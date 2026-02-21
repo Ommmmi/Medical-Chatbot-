46a1d2e229a2034ef15198c68d0526fa1cb39256
ommmmi <ritul.ommi@gmail.com>
2026-02-21 19:00:12 +0530

    requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot",
+    version="0.0.1",
+    author="Ritul",
+    author_email="ritul.ommi@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
